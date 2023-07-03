aa2d1bc045b78f035c817aa9a98fd9b8f27aa442
mjresch19 <mjresch19@gmail.com>
2023-07-03 15:30:57 -0400

    TODO move this login page from home.dart to login.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,18 +1,144 @@
 import 'package:flutter/material.dart';
+import 'package:pgn_app/Controllers/AnimationAppearController.dart';
+import 'package:pgn_app/Controllers/AnimationAppearController.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                colors: [
+                  Colors.redAccent,
+                  Colors.redAccent,
+                  Colors.redAccent
+                ]
+            )
+        ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text('Home Screen')
+            SizedBox(height: 80,),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  AnimAppear(delay: 250, child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 40),),),
+                  SizedBox(height: 10,),
+                  AnimAppear(delay: 400, child: Text("Welcome Back", style: TextStyle(color: Colors.white, fontSize: 18),)),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60))
+                ),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 60,),
+                        AnimAppear(delay: 400, child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [BoxShadow(
+                                  color: Color.fromRGBO(225, 95, 27, .3),
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10)
+                              )]
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey))
+                                ),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      hintText: "Email or Phone number",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey))
+                                ),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      hintText: "Password",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )),
+                        SizedBox(height: 40,),
+                        AnimAppear(delay: 375, child: Text("Forgot Password?", style: TextStyle(color: Colors.grey),)),
+                        SizedBox(height: 40,),
+                        AnimAppear(delay: 375, child: Container(
+                          height: 50,
+                          margin: EdgeInsets.symmetric(horizontal: 50),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.orange[900]
+                          ),
+                          child: Center(
+                            child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                          ),
+                        )),
+                        SizedBox(height: 50,),
+                        AnimAppear(delay: 425,child: Text("Continue with social media", style: TextStyle(color: Colors.grey),)),
+                        SizedBox(height: 30,),
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: AnimAppear(delay: 450, child: Container(
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.blue
+                                ),
+                                child: Center(
+                                  child: Text("Facebook", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                                ),
+                              )),
+                            ),
+                            SizedBox(width: 30,),
+                            Expanded(
+                              child: AnimAppear(delay: 400,child: Container(
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.black
+                                ),
+                                child: Center(
+                                  child: Text("Github", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                                ),
+                              )),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
