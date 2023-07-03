a8dddf99c37f37bdfda311b6c49c9bacd3385b24
mjresch19 <mjresch19@gmail.com>
2023-07-03 15:57:22 -0400

    Redid Login Page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pgn_app/Controllers/AnimationAppearController.dart';
 import 'package:pgn_app/Buttons/ambersubmitbutton.dart';
 
+
 class Login extends StatefulWidget {
   @override
   _LoginState createState() => _LoginState();
@@ -11,123 +12,109 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.redAccent,
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            children: <Widget>[
-              AnimAppear(
-                delay: 250,
-                child: Container(
-                    margin: EdgeInsets.fromLTRB(20,0,20,0),
-                    color: Colors.redAccent,
-                    width: double.infinity,
-                    height: 200,
-                    child: Align(
-                      alignment: Alignment.center,
-                        child: Text(
-                          "PGN\nLogin",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 50,
-                            letterSpacing: 5.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white
-                      ),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                colors: [
+                  Color.fromARGB(255, 255, 13, 1),
+                  Color.fromARGB(255, 167, 41, 1),
+                  Color.fromARGB(255, 242, 95, 1),
+                ]
+            )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 80,),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  AnimAppear(delay: 250, child: Text("PGN Login", style: TextStyle(color: Colors.white, fontSize: 40),),),
+                  SizedBox(height: 10,),
+                  AnimAppear(delay: 400, child: Text("Welcome Back", style: TextStyle(color: Colors.white, fontSize: 18),)),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60))
+                ),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 60,),
+                        AnimAppear(delay: 400, child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [BoxShadow(
+                                  color: Color.fromRGBO(225, 95, 27, .3),
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10)
+                              )]
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey))
+                                ),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      hintText: "Email or Phone number",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey))
+                                ),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      hintText: "Password",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )),
+                        SizedBox(height: 40,),
+                        AnimAppear(delay: 375, child: Text("New User? Sign Up!", style: TextStyle(color: Colors.grey),)),
+                        SizedBox(height: 40,),
+                        AnimAppear(delay: 375, child: Container(
+                          height: 50,
+                          margin: EdgeInsets.symmetric(horizontal: 50),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.amberAccent[700],
+                          ),
+                          child: Center(
+                            child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                          ),
+                        )),
+                        SizedBox(height: 50,),
+                      ],
                     ),
                   ),
                 ),
               ),
-              AnimAppear(
-                  delay: 250,
-                  child: Container(
-                      margin: EdgeInsets.fromLTRB(10, 20, 10, 0),
-                      color: Colors.redAccent,
-                      width: 100,
-                      height: 20,
-                      child: Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                              "Email",
-                              style: TextStyle(
-                                fontSize: 15,
-                                letterSpacing: 2.0,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              )
-                          )
-                      )
-                  )
-              ),
-              SizedBox(height: 8.0),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10,0,10,0),
-                child: AnimAppear(
-                    delay: 250,
-                    child: SizedBox(
-                      width: 300,
-                      height: 50,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          fillColor: Colors.white70,
-                          filled: true,
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                    )
-                ),
-              ),
-              AnimAppear(
-                  delay: 250,
-                  child: Container(
-                      margin: EdgeInsets.fromLTRB(10, 20, 10, 0),
-                      color: Colors.redAccent,
-                      width: 100,
-                      height: 20,
-                      child: Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                              "Password",
-                              style: TextStyle(
-                                fontSize: 15,
-                                letterSpacing: 2.0,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              )
-                          )
-                      )
-                  )
-              ),
-              SizedBox(height: 8.0),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10,0,10,0),
-                child: AnimAppear(
-                    delay: 250,
-                    child: SizedBox(
-                      width: 300,
-                      height: 50,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          fillColor: Colors.white70,
-                          filled: true,
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                    )
-                ),
-              ),
-              SizedBox(height:50.0),
-              AnimAppear(
-                delay: 250,
-                child: AmberSubmitButton(
-                    message: "Login!",
-                    onTap: () {
-                      Navigator.pushNamed(context, '/home');
-                    },
-                ),
-              ),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
