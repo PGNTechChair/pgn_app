491e327daf635f19d49b69a5b1bef4d05258094a
mjresch19 <mjresch19@gmail.com>
2023-07-30 15:29:50 -0400

    Added sign out functionality to sidebar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -71,7 +71,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
         body: Stack(
           children: [
             Center(
-              child: Text('Your Main Content'),
+              child: Text('Home'),
             ),
             if (isSidebarOpen)
               GestureDetector(
--- lib/services/side_menu_tile.dart
+++ lib/services/side_menu_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pgn_app/services/auth.dart';
 
 class SideMenuTile extends StatelessWidget {
 
@@ -9,23 +10,54 @@ class SideMenuTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal:18.0),
-          child: Divider(
-            color: Colors.white24,
-            thickness: 1 ,
+
+    final AuthService _auth = AuthService();
+
+    if (sideMenuOption == "Sign Out")
+    {
+      return Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal:18.0),
+            child: Divider(
+              color: Colors.white24,
+              thickness: 1 ,
+            ),
           ),
-        ),
-        ListTile(
-            leading: iconName,
-            title: Text(
-                sideMenuOption,
-                style: TextStyle(color: Colors.white)
-            )
-        ),
-      ],
-    );
+          ListTile(
+              leading: iconName,
+              title: Text(
+                  sideMenuOption,
+                  style: TextStyle(color: Colors.white),
+              ),
+              onTap: () async {
+                await _auth.signOut();
+              },
+          ),
+        ],
+      );
+    }
+    else {
+      return Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal:18.0),
+            child: Divider(
+              color: Colors.white24,
+              thickness: 1 ,
+            ),
+          ),
+          ListTile(
+              leading: iconName,
+              title: Text(
+                  sideMenuOption,
+                  style: TextStyle(color: Colors.white)
+              )
+          ),
+        ],
+      );
+    }
+
+
   }
 }
