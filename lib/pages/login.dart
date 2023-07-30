c0c69c360f77802bfb5e28aa24bb25af6debddd7
mjresch19 <mjresch19@gmail.com>
2023-07-29 20:29:30 -0400

    Preserved round edges through scrollable view of the login page.

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -82,149 +82,155 @@ class _LoginState extends State<Login> {
                     topRight: Radius.circular(60)
                     ),
                 ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                    topRight: Radius.circular(60),
+                  ),
                 child: SingleChildScrollView(
-                  child: Padding(
-                    padding: EdgeInsets.all(30),
-                    child: Form(
-                      key: _formKey,
-                      child: Column(
-                        children: <Widget>[
-                          SizedBox(height: 60,),
-                          Container(
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: [BoxShadow(
-                                    color: Color.fromRGBO(225, 95, 27, .3),
-                                    blurRadius: 20,
-                                    offset: Offset(0, 10)
-                                )]
-                            ),
-                            child: Column(
-                              children: <Widget>[
-                                Container(
-                                  padding: EdgeInsets.all(10),
-                                  decoration: BoxDecoration(
-                                      border: Border(bottom: BorderSide(color: Colors.grey))
-                                  ),
-                                  child: TextFormField(
-                                    validator: (String? val) {
-                                      if (val == null || val.isEmpty)
-                                      {
-                                        return "Please Enter an Email";
-                                      }
+                    child: Padding(
+                      padding: EdgeInsets.all(30),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          children: <Widget>[
+                            SizedBox(height: 60,),
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [BoxShadow(
+                                      color: Color.fromRGBO(225, 95, 27, .3),
+                                      blurRadius: 20,
+                                      offset: Offset(0, 10)
+                                  )]
+                              ),
+                              child: Column(
+                                children: <Widget>[
+                                  Container(
+                                    padding: EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                        border: Border(bottom: BorderSide(color: Colors.grey))
+                                    ),
+                                    child: TextFormField(
+                                      validator: (String? val) {
+                                        if (val == null || val.isEmpty)
+                                        {
+                                          return "Please Enter an Email";
+                                        }
 
-                                      final emailRegExp = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
-                                      if (!emailRegExp.hasMatch(val)) {
-                                        return 'Please enter an email address.';
-                                      }
-                                      return null;
-                                    },
-                                    onChanged: (val)
-                                    {
-                                      setState(() => email = val);
-                                    },
-                                    decoration: InputDecoration(
-                                        hintText: "Email",
-                                        hintStyle: TextStyle(color: Colors.grey),
-                                        border: InputBorder.none
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                  padding: EdgeInsets.all(10),
-                                  decoration: BoxDecoration(
-                                      border: Border(bottom: BorderSide(color: Colors.grey))
-                                  ),
-                                  child: TextFormField(
-                                    obscureText: true,
-                                    validator: (String? val) {
-                                      if (val != null && val.length < 6)
+                                        final emailRegExp = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+                                        if (!emailRegExp.hasMatch(val)) {
+                                          return 'Please enter an email address.';
+                                        }
+                                        return null;
+                                      },
+                                      onChanged: (val)
                                       {
-                                        return "Please Enter a Password with 6+ characters.";
-                                      }
-                                      return null;
-                                    },
-                                    onChanged: (val)
-                                    {
-                                      setState(() => password = val);
-                                    },
-                                    decoration: InputDecoration(
-                                        hintText: "Password",
-                                        hintStyle: TextStyle(color: Colors.grey),
-                                        border: InputBorder.none
+                                        setState(() => email = val);
+                                      },
+                                      decoration: InputDecoration(
+                                          hintText: "Email",
+                                          hintStyle: TextStyle(color: Colors.grey),
+                                          border: InputBorder.none
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 40,),
-                          Container(
-                            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                            child: RichText(
-                              text: TextSpan(
-                                text: "New User? ",
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.grey,
-                                ),
-                                children: <TextSpan> [
-                                  TextSpan(
-                                    text: "Sign Up!",
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.grey,
-                                      decoration: TextDecoration.underline,
-                                      decorationThickness: 2.0,
+                                  Container(
+                                    padding: EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                        border: Border(bottom: BorderSide(color: Colors.grey))
                                     ),
-                                    recognizer: TapGestureRecognizer()
-                                      ..onTap = () {
-                                        widget.toggleView();
-                                        // Navigator.pushNamed(context, '/signup');
-                                      }
+                                    child: TextFormField(
+                                      obscureText: true,
+                                      validator: (String? val) {
+                                        if (val != null && val.length < 6)
+                                        {
+                                          return "Please Enter a Password with 6+ characters.";
+                                        }
+                                        return null;
+                                      },
+                                      onChanged: (val)
+                                      {
+                                        setState(() => password = val);
+                                      },
+                                      decoration: InputDecoration(
+                                          hintText: "Password",
+                                          hintStyle: TextStyle(color: Colors.grey),
+                                          border: InputBorder.none
+                                      ),
                                     ),
-                                  ]
+                                  ),
+                                ],
                               ),
                             ),
-                          ),
-                          SizedBox(height: 40,),
-                          Align(
-                          alignment: Alignment.center,
-                            child: AmberSubmitButton(
-                              message: "Login!",
-                              onTap: () async
-                              {
-                                if (_formKey.currentState!.validate()) {
-                                  setState(() => loading = true);
-                                  dynamic result = await _authService
-                                      .signInWithEmailAndPassword(
-                                      email, password);
-                                  if (result == null) {
-                                      setState(() {
-                                          error = "Could not sign in with invalid credentials.";
-                                          loading = false;
-                                      });
+                            SizedBox(height: 40,),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                              child: RichText(
+                                text: TextSpan(
+                                  text: "New User? ",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.grey,
+                                  ),
+                                  children: <TextSpan> [
+                                    TextSpan(
+                                      text: "Sign Up!",
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.grey,
+                                        decoration: TextDecoration.underline,
+                                        decorationThickness: 2.0,
+                                      ),
+                                      recognizer: TapGestureRecognizer()
+                                        ..onTap = () {
+                                          widget.toggleView();
+                                          // Navigator.pushNamed(context, '/signup');
+                                        }
+                                      ),
+                                    ]
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 40,),
+                            Align(
+                            alignment: Alignment.center,
+                              child: AmberSubmitButton(
+                                message: "Login!",
+                                onTap: () async
+                                {
+                                  if (_formKey.currentState!.validate()) {
+                                    setState(() => loading = true);
+                                    dynamic result = await _authService
+                                        .signInWithEmailAndPassword(
+                                        email, password);
+                                    if (result == null) {
+                                        setState(() {
+                                            error = "Could not sign in with invalid credentials.";
+                                            loading = false;
+                                        });
+                                    }
                                   }
                                 }
-                              }
+                              ),
                             ),
-                          ),
-                          SizedBox(height: 15.0),
-                          Align(
-                            alignment: Alignment.center,
-                            child: Text(
-                              error,
-                              style: TextStyle(color: Colors.black, fontSize: 14.0),
+                            SizedBox(height: 15.0),
+                            Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                error,
+                                style: TextStyle(color: Colors.black, fontSize: 14.0),
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
