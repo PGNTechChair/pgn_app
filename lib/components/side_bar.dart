1e66a44b0d295c42f60fa50d115cd4d8576ce86f
mjresch19 <mjresch19@gmail.com>
2023-07-30 09:59:34 -0400

    has ability to pass in user information to sidebar.

--- lib/components/side_bar.dart
+++ lib/components/side_bar.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pgn_app/models/user.dart';
+import 'package:provider/provider.dart';
 
 class SideBar extends StatefulWidget {
   const SideBar({super.key});
@@ -8,8 +11,12 @@ class SideBar extends StatefulWidget {
 }
 
 class _SideBarState extends State<SideBar> {
+
   @override
   Widget build(BuildContext context) {
+
+    final userData = Provider.of<UserData?>(context);
+
     return Scaffold(
       body: SafeArea(
         child: Container(
@@ -17,15 +24,23 @@ class _SideBarState extends State<SideBar> {
           height: double.infinity,
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              colors: [Colors.yellow, Color(0xffad9c00)],
+              colors: [Color(0xFF8B0000), Color(0xFFB71C1C)],
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
             )
           ),
-          child: Column (
-            children: [
-
-            ],
+          child: SafeArea(
+            child: Column (
+              children: [
+                ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.amber[800],
+                    child: Icon(Icons.person, color: Colors.white)
+                  ),
+                  title: Text(userData!.firstName)
+                )
+              ],
+            ),
           )
         ),
       ),
