25868e745699c55a0018a7688570511d43703945
mjresch19 <mjresch19@gmail.com>
2023-08-25 23:48:23 -0400

    Finished President Statement and thus the first story page

--- lib/pages/home/story1page.dart
+++ lib/pages/home/story1page.dart
@@ -239,7 +239,104 @@ class _StoryPage1State extends State<StoryPage1> {
                       ),
                     ),
                   ),
-                  SizedBox(height: MediaQuery.of(context).size.height * (1/8)),
+                  Divider(
+                      color: Colors.indigo[200],
+                      thickness: 2.0,
+                      height: MediaQuery.of(context).size.height * (1/8)
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 100,
+                    child: Text(
+                      "A Statement From Our President",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.bangers(
+                        fontSize: 36,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * (1/32)),
+                  Container(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 5), // Inner gold border
+                          borderRadius: BorderRadius.circular(5)
+                      ),
+                      child: Image.asset(
+                        "assets/JoeyDeRose.png",
+                        width: MediaQuery.of(context).size.width- 100,
+                        height: MediaQuery.of(context).size.width- 100,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * (1/32)),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 50,
+                    child: Text(
+                      "Joey DeRose",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.bangers(
+                        fontSize: 40,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 50,
+                    child: Text(
+                      "PGN President - 2023",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.bangers(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * (1/32)),
+                  Card(
+                    color: Colors.grey[200],
+                    elevation: 10,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'PGN has given me the opportunity to develop not just as a professional, but also as a person. I personally dealt with a rapidly changing environment around me coming into college. PGN helped me adjust to this change. Albeit the pledge semester was difficult and even being president has its challenges, but I would be nowhere near where I am today if it wasn\'t for the amazing upperclassmen that raised a professional out of me along with the amazing support system I have today.',
+                            style: GoogleFonts.montserrat(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            '\nI can say with a full heart that this community is bonded by passion and shares the goal of achievement. I am always amazed by those around me that are continually pushing the boundaries and limitations day after day.',
+                            style: GoogleFonts.montserrat(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            "\nI hope that I can prove to you that PGN fosters both creative and innovative ideas of all its members. We will enable your ideas to flourish into fruition by providing a variety of workshops, along with helpful and meaningful connections that can be utilized to tackle the often scary unknowns of college life.",
+                            style: GoogleFonts.montserrat(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            "\nIf you have reached this far in my note, thank you for taking the time to learn more about our organization. I hope we can leave as much an impact on you as it has on me.",
+                            style: GoogleFonts.montserrat(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * (1/32)),
                 ],
               ),
             ),
